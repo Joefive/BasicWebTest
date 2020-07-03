d8b8a1af463d55d91395596262357c458f14ff5e
Joefive <joefive@yeah.net>
2020-07-03 11:32:49 +0800

    new

--- src/com/sunrise/annotation/AnnotationUse.java
+++ src/com/sunrise/annotation/AnnotationUse.java
@@ -1,13 +1,15 @@
 package com.sunrise.annotation;
 
+import java.lang.reflect.Method;
+
 /**
  * 目的：不改变任何代码，可以任意创建对象，可以调用任意方法
  * 自定义注解的作用：注解后期作用主要用来替换配置文件
  */
 
-@AnnoUser(className = "com.sunrise.annotation.demo1",methodName = "show")
+@AnnoUser(className = "com.sunrise.annotation.AnnoDemo1",methodName = "show")
 public class AnnotationUse {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //1.获取类的字节码文件
         Class<AnnotationUse> au = AnnotationUse.class;
         //2.获取注解对象
@@ -17,5 +19,15 @@ public class AnnotationUse {
         String methodName = an.methodName();
         System.out.println(className);
         System.out.println(methodName);
+
+        //创建对象及调用方法
+        Class cls = Class.forName(className);
+        //4.创建对象
+        Object obj = cls.newInstance();
+        //5.获取方法
+        Method method = cls.getMethod(methodName);
+        //6.执行方法
+        method.invoke(obj);
+
     }
 }
