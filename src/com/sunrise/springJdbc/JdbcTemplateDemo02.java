9002dd599166bb696549ee69e64e4f09820b9d0c
Joefive <joefive@yeah.net>
2020-09-07 16:55:13 +0800

    new

--- src/com/sunrise/springJdbc/JdbcTemplateDemo02.java
+++ src/com/sunrise/springJdbc/JdbcTemplateDemo02.java
@@ -1,7 +1,16 @@
 package com.sunrise.springJdbc;
 
+import com.sunrise.domain.Emp;
 import com.sunrise.utils.DruidUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * JdbcTemplate常用方法：
@@ -18,7 +27,9 @@ public class JdbcTemplateDemo02 {
         methodOne(1);
         methodTwo();
         methodThree("白龙马");
-
+        methodFour(1);
+        methodFive();
+        methodSix();
     }
 
     /**
@@ -61,5 +72,72 @@ public class JdbcTemplateDemo02 {
         } else {
             System.out.println("方法methodThree执行【失败】！");
         }
+    }
+
+    /**
+     * 需求4，查询ID为1的数据并封装到MAP集合中
+     * 查询长度只能为1，只能查询出1行数据，列=key，值=value
+     */
+    public static void methodFour(int numId) {
+        String sql = "SELECT * FROM EMP WHERE ID=?";
+        Map<String, Object> stringObjectMap = template.queryForMap(sql, numId);
+        System.out.println("stringObjectMap : " + stringObjectMap);
+        System.out.println("**********需求4执行完成**********");
+    }
+
+    /**
+     * 需求5，查询出一张表所有数据封装到LIST集合中
+     * 每行数据封装成MAP集合，再将所有的MAP集合封装到
+     */
+    public static void methodFive() {
+        String sql = "SELECT * FROM EMP";
+        List<Map<String, Object>> list = template.queryForList(sql);
+        int count = 1;
+        for (Map<String, Object> stringObjectMap : list) {
+            System.out.println("第" + count + "行:" + stringObjectMap);
+            count++;
+            System.out.println("**********需求5执行完成需求5执行完成**********");
+        }
+    }
+
+    /**【复杂版】
+     * 需求6，查询结果封装到Bean中然后再次封装到List中
+     */
+    public static void methodSix() {
+        String sql = "SELECT * FROM EMP";
+        //传递sql，传递封装对象
+        List<Emp> queryList = template.query(sql, new RowMapper<Emp>() {
+            @Override
+            public Emp mapRow(ResultSet resultSet, int i) throws SQLException {
+                Emp emp = new Emp();
+                int id = emp.getId();
+                String name = emp.getName();
+                double salary = emp.getSalary();
+                String gender = emp.getGender();
+                Date join_date = emp.getJoin_date();
+                int dept_id = emp.getDept_id();
+
+                emp.setId(id);
+                emp.setDept_id(dept_id);
+                emp.setJoin_date(join_date);
+                emp.setSalary(salary);
+                emp.setGender(gender);
+                emp.setName(name);
+
+                return emp;
+            }
+        });
+        for (Emp emp : queryList) {
+            System.out.println(emp);
+        }
+    }
+
+    /**【简化版】
+     * 需求6，调用BeanPropertyRowMapper实现类，省略Bean中复杂的get与set方法
+     */
+
+    public static void methodSixSimple(){
+        String sql = "SELECT * FROM EMP";
+
     }
 }
